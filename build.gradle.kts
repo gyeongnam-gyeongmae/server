20f63204db7faf1125f44a2373a99927bba449fe
haroya01 <haroya01@naver.com>
2023-09-04 20:11:22 +0900

    feat : querydsl환경 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
+import com.ewerk.gradle.plugins.tasks.QuerydslCompile
+
 plugins {
     java
     id("org.springframework.boot") version "2.7.14"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
+
 }
 
 group = "megabrain"
 version = "0.0.1-SNAPSHOT"
+
+val queryDslVersion = "5.0.0"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -14,6 +20,12 @@ java {
 
 repositories {
     mavenCentral()
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 dependencies {
@@ -29,8 +41,42 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testAnnotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
+
+    implementation("com.querydsl:querydsl-jpa:${queryDslVersion}")
+    annotationProcessor("com.querydsl:querydsl-apt:${queryDslVersion}")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+
+val querydslDir = "$buildDir/generated/querydsl" //
+
+querydsl {
+    jpa = true
+    querydslSourcesDir = querydslDir
+}
+// 소스 루트 지정
+sourceSets {
+    main {
+        java {
+            srcDir(querydslDir)
+
+        }
+    }
+}
+
+configurations {
+    named("querydsl") {
+        extendsFrom(configurations.compileClasspath.get())
+    }
+}
+
+tasks.withType<QuerydslCompile> {
+    options.annotationProcessorPath = configurations.querydsl.get()
+}
+
