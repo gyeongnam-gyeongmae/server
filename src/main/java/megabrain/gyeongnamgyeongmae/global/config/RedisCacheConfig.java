19708fdca8e6b860e00d6092cf566031bac2bd51
puleugo <puleugo@gmail.com>
2023-09-15 12:49:33 +0900

    comment: redis 임시 비활성화

--- src/main/java/megabrain/gyeongnamgyeongmae/global/config/RedisCacheConfig.java
+++ src/main/java/megabrain/gyeongnamgyeongmae/global/config/RedisCacheConfig.java
@@ -1,6 +1,5 @@
 package megabrain.gyeongnamgyeongmae.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +7,9 @@ import org.springframework.context.annotation.Configuration;
 @EnableCaching
 public class RedisCacheConfig {
 
-  @Value("${spring.redis.cache.host}")
-  private String host;
-
-  @Value("${spring.redis.cache.port}")
-  private int port;
+  //  @Value("${spring.redis.cache.host}")
+  //  private String host;
+  //
+  //  @Value("${spring.redis.cache.port}")
+  //  private int port;
 }
