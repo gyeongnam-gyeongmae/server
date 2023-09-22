3b678230753dd580c4fc7d30ee341ddedb4b9198
haroya01 <haroya01@naver.com>
2023-09-22 11:26:37 +0900

    fix: 사용하지 않는 import 제거

--- src/main/java/megabrain/gyeongnamgyeongmae/domain/image/Service/ImageService.java
+++ src/main/java/megabrain/gyeongnamgyeongmae/domain/image/Service/ImageService.java
@@ -1,6 +1,6 @@
 package megabrain.gyeongnamgyeongmae.domain.image.Service;
 
-import megabrain.gyeongnamgyeongmae.domain.image.dto.ImageUploadDto;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
