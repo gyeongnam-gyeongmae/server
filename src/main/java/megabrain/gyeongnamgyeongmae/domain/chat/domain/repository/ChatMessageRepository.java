9a7e9ca19a91e9eba06367cb996fb0fb37170e10
puleugo <puleugo@gmail.com>
2023-09-24 17:33:14 +0900

    feat: 채팅관련 DAO 구현

---
+++ src/main/java/megabrain/gyeongnamgyeongmae/domain/chat/domain/repository/ChatMessageRepository.java
@@ -0,0 +1,9 @@
+package megabrain.gyeongnamgyeongmae.domain.chat.domain.repository;
+
+import java.util.List;
+import megabrain.gyeongnamgyeongmae.domain.chat.domain.entity.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+  List<ChatMessage> getChatMessageByRoomIdOrderByCreatedAtAsc(Long roomId);
+}
