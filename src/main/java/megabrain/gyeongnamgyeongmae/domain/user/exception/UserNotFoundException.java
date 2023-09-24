4359f1d2ce65f022afb297634f34a4250025a249
puleugo <puleugo@gmail.com>
2023-09-24 17:34:12 +0900

    feat: UserNotFoundException 예외처리 추가

---
+++ src/main/java/megabrain/gyeongnamgyeongmae/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package megabrain.gyeongnamgyeongmae.domain.user.exception;
+
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException() {}
+
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+
+  public UserNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
--- src/main/java/megabrain/gyeongnamgyeongmae/global/advice/ExceptionAdvice.java
+++ src/main/java/megabrain/gyeongnamgyeongmae/global/advice/ExceptionAdvice.java
@@ -5,6 +5,7 @@ import megabrain.gyeongnamgyeongmae.domain.authentication.exception.OAuthLoginEx
 import megabrain.gyeongnamgyeongmae.domain.authentication.exception.OAuthVendorNotFoundException;
 import megabrain.gyeongnamgyeongmae.domain.authentication.exception.UnAuthenticatedException;
 import megabrain.gyeongnamgyeongmae.domain.user.exception.DuplicateAuthVendorUserId;
+import megabrain.gyeongnamgyeongmae.domain.user.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -22,6 +23,11 @@ public class ExceptionAdvice {
 
   @ExceptionHandler(OAuthVendorNotFoundException.class)
   public ResponseEntity<String> oAuthVendorNotFoundException(OAuthVendorNotFoundException error) {
+    return new ResponseEntity<>(error.getMessage(), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(UserNotFoundException.class)
+  public ResponseEntity<String> userNotFoundException(UserNotFoundException error) {
     return new ResponseEntity<>(error.getMessage(), HttpStatus.NOT_FOUND);
   }
 
