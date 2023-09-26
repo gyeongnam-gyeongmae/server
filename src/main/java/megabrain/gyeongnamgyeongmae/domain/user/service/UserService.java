dff2df90cb6837ff8288da0a1515b3a3c0919f10
haroya01 <haroya01@naver.com>
2023-09-26 15:27:24 +0900

    feat: 전체 유저 목록 반환 추가

--- src/main/java/megabrain/gyeongnamgyeongmae/domain/user/service/UserService.java
+++ src/main/java/megabrain/gyeongnamgyeongmae/domain/user/service/UserService.java
@@ -1,5 +1,6 @@
 package megabrain.gyeongnamgyeongmae.domain.user.service;
 
+import java.util.List;
 import megabrain.gyeongnamgyeongmae.domain.user.domain.entity.User;
 
 public interface UserService {
@@ -9,4 +10,6 @@ public interface UserService {
   User findUserById(Long Id);
 
   Long getIdByAuthVendorUserId(String authVendorUserId);
+
+  List<User> findAllUser();
 }
