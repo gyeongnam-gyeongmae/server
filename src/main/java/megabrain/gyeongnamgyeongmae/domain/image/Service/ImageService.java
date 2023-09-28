0d8ba8f64079db5537509ce2ad9a04720bf144f1
haroya01 <haroya01@naver.com>
2023-09-28 18:47:23 +0900

    refactor: image의 url 반환 로직과 google format 적용

--- src/main/java/megabrain/gyeongnamgyeongmae/domain/image/Service/ImageService.java
+++ src/main/java/megabrain/gyeongnamgyeongmae/domain/image/Service/ImageService.java
@@ -1,13 +1,15 @@
 package megabrain.gyeongnamgyeongmae.domain.image.Service;
 
-
-import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.List;
+import megabrain.gyeongnamgyeongmae.domain.image.domain.entity.Image;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageService {
 
-    void uploadImage(List<MultipartFile> images, String from, Long id) throws IOException;
+  void uploadImage(List<MultipartFile> images, String from, Long id) throws IOException;
 
+  List<Image> findImageByAuctionItemId(Long id);
+
+  List<String> findImageByAuctionItemIdBackUrls(Long id);
 }
