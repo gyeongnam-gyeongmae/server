5d7cfef7a1df2d364fd30dfb2ec0c2ba9ad2d10d
haroya01 <haroya01@naver.com>
2023-10-02 01:02:31 +0900

    docs: 이미지 관련 swagger수정

--- src/main/java/megabrain/gyeongnamgyeongmae/domain/image/Controller/ImageController.java
+++ src/main/java/megabrain/gyeongnamgyeongmae/domain/image/Controller/ImageController.java
@@ -3,17 +3,19 @@ package megabrain.gyeongnamgyeongmae.domain.image.Controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import megabrain.gyeongnamgyeongmae.domain.image.Service.ImageService;
-import megabrain.gyeongnamgyeongmae.domain.image.domain.entity.UploadType;
+import megabrain.gyeongnamgyeongmae.domain.image.dto.UploadType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name = "이미지 업로드", description = "이미지 업로드 관련 api")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/{from}/{id}/images/")
@@ -24,9 +26,7 @@ public class ImageController {
   @PostMapping(
       value = "/upload",
       consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-  @Operation(
-      summary = "이미지 업로드 ",
-      description = "이미지 업로드, from : AuctionItem, id : AuctionItem id 를 입력하시면 됩니다.")
+  @Operation(summary = "이미지 업로드 ", description = "이미지 업로드, from : AuctionItem, id : 해당 경매품의 id")
   @ApiResponses(
       value = {
         @ApiResponse(responseCode = "200", description = "이미지 업로드 성공"),
