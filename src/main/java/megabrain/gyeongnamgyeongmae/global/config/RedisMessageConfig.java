1bed6071ff9e540d6e30f4d28ca63dd5fd785f0a
puleugo <puleugo@gmail.com>
2023-10-02 14:39:13 +0900

    fix: redis host 문제 롤백

--- src/main/java/megabrain/gyeongnamgyeongmae/global/config/RedisMessageConfig.java
+++ src/main/java/megabrain/gyeongnamgyeongmae/global/config/RedisMessageConfig.java
@@ -2,7 +2,6 @@ package megabrain.gyeongnamgyeongmae.global.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Getter
@@ -10,8 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class RedisMessageConfig {
 
-  @Value("${spring.redis.host}")
-  private String host;
+  private String host = "gg-redis";
 
   private int port = 6379;
 }
